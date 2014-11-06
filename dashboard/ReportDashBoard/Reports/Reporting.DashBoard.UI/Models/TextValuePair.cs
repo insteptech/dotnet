c49a91cecf03d9c6eddc2bc0c95ab8c2f1dd6116
Mahesh <mahesh@insteptechnologies.com>
2014-11-06 16:32:01 +0530

    deleted

---
+++ dashboard/ReportDashBoard/Reports/Reporting.DashBoard.UI/Models/TextValuePair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reporting.DashBoard.UI.Models
+{
+    public class TextValuePair
+    {
+        //Key value pair for list
+        public string Text { get; set; }
+        public int Value { get; set; }
+    }
+}
